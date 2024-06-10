8417f579b51d16d1fe83e7f808bd710f4f0ef2de
AgusJara <ajaralambides@udesa.edu.ar>
2024-06-09 22:40:06 -0300

    Hice la seecion de ropa para hombres y mujeres con la informacacion de la API, tambien tuve que hacer modificaciones en el css y html, falta adaptarlo a responsive pero lo hago mas adelante

--- js/index.js
+++ js/index.js
@@ -4,22 +4,67 @@ fetch ('https://fakestoreapi.com/products')
     })
     .then(function (data) {
         // console.log(data);
-        let arrayDeAccesorios = data;
-        let divSeccionAcc = document.querySelector('.subcontainer');
-        let accesorios = [];
-        console.log(arrayDeAccesorios);
+        
+//<---------------------Seccion Accesorios-------------------->//
 
-        for (let i = 5; i < 8; i++) {
+let arrayDeProductos = data;
+let divSeccionAcc = document.querySelector('.subcontainer');
+let accesorios = [];
+        // console.log(arrayDeProductos);
+
+    for (let i = 4; i < 8; i++) {
             accesorios += `<div>
-                        <img src="${arrayDeAccesorios[i].image}" alt="${arrayDeAccesorios[i].title}">
-                        <h4>${arrayDeAccesorios[i].title}</h4>
-                        <p>${arrayDeAccesorios[i].description}</p>
-                        <p>Precio: ${arrayDeAccesorios[i].price}</p>
+                        <img src="${arrayDeProductos[i].image}" alt="${arrayDeProductos[i].title}">
+                        <h4>${arrayDeProductos[i].title}</h4>
+                        <p>${arrayDeProductos[i].description}</p>
+                        <p>Precio: $${arrayDeProductos[i].price}</p>
                         <a href="./producto.html">Ver más</a>
                         </div>`
-        }     
+    }     
 
         divSeccionAcc.innerHTML = accesorios;
+
+//<---------------------Seccion Tecnologia-------------------->//
+
+        
+
+//<---------------------Seccion Ropa Hombre-------------------->//
+
+let divSeccionRopaH = document.querySelector('.subcontainer3');
+        let ropaH = [];
+        console.log(arrayDeProductos);
+
+        for (let i = 0; i < 4; i++) {
+            ropaH += `<div>
+                        <img src="${arrayDeProductos[i].image}" alt="${arrayDeProductos[i].title}">
+                        <h4>${arrayDeProductos[i].title}</h4>
+                        <p>${arrayDeProductos[i].description}</p>
+                        <p>Precio: $${arrayDeProductos[i].price}</p>
+                        <a href="./producto.html">Ver más</a>
+                        </div>`
+    }     
+
+        divSeccionRopaH.innerHTML = ropaH;
+
+//<---------------------Seccion Ropa mujer-------------------->//
+
+let divSeccionRopaM = document.querySelector('.subcontainer4');
+let ropaM = [];
+console.log(arrayDeProductos);
+
+for (let i = 14; i < 20; i++) {
+    ropaM += `<div>
+                <img src="${arrayDeProductos[i].image}" alt="${arrayDeProductos[i].title}">
+                <h4>${arrayDeProductos[i].title}</h4>
+                <p>${arrayDeProductos[i].description}</p>
+                <p>Precio: $${arrayDeProductos[i].price}</p>
+                <a href="./producto.html">Ver más</a>
+                </div>`
+    }
+
+        divSeccionRopaM.innerHTML = ropaM;
+
+
     })
     .catch(function(e){
         console.log(e);
