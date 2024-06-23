8da2efbf343cf399591456aa12cf72d02c08b5a6
MarcoMambrettiUdesa <mmambretti@udesa.edu.ar>
2024-06-23 09:49:44 -0300

    modifique el login

--- js/login.js
+++ js/login.js
@@ -6,12 +6,6 @@
 let form = document.querySelector ('form');
 let email = document.querySelector('#email');
 let pass = document.querySelector('#password');
-let passConfirm = document.querySelector('#passConfirm');
-let idCheck = document.querySelector ('#checkbox');
-let emailErr = document.querySelector ('.emailErr');
-let passErr = document.querySelector ('.passErr');
-let passConfErr = document.querySelector('.passConfErr');
-let errCheck = document.querySelector ('.errCheck');
 let usuario= {};
 let usuarios=[];
 
@@ -29,33 +23,28 @@ form.addEventListener ('submit', function(e){
 })
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
-
-    form.addEventListener("submit", function(event) {
-        const email = document.getElementById("email").value.trim();
-        const password = document.getElementById("password").value.trim();
-
-        if (email === "") {
-            alert("Por favor complete el campo email");
-            event.preventDefault();
-            return;
+document.addEventListener ('DOMContentLoaded', function () {
+    let pass = document.querySelector('#password');
+    let passErr = document.querySelector ('.passErr');
+    
+    pass.addEventListener ('input', function() {
+        if (pass.value == ''){
+            passErr.innerText ='Por favor complete el campo';
+        } else if (pass.value.length <= 6){
+            passErr.innerText ='Debe ingresar al menos 6 caracteres';
+        } else {
+            passErr.innerText = '';
         }
-
-        if (password === "") {
-            alert("Por favor complete el campo contraseña");
-            event.preventDefault();
-            return;
+    })
+    
+    email.addEventListener('input', function(){
+        if (email.value =='') {
+           emailErr.innerText = 'Por favor complete el campo';
+        } else{
+            emailErr.innerText = '';
         }
+    })
+    
+    })
 
-        if (password.length < 6) {
-            alert("La contraseña debe tener al menos 6 caracteres");
-            event.preventDefault();
-            return;
-        }
-
-        localStorage.setItem("email", email);
-    });
-});
-
-
+    
