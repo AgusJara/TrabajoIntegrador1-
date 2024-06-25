53eed562297aa252a3c8a54a794522cb7d5e1044
AgusJara <ajaralambides@udesa.edu.ar>
2024-06-24 21:07:25 -0300

    Borre unos comentarios de codigo

--- js/cart.js
+++ js/cart.js
@@ -6,36 +6,6 @@ let finalizar = document.querySelector('.finalizar')
 
 console.log(recuperoStorage);
 
-
-// if (recuperoStorage !== null){
-//     storageToParse.forEach(id => {
-//         fetch(`https://fakestoreapi.com/products/${id}`)
-//                 .then(res=>res.json())
-//                 .then(json=>{
-//                     let product = `
-//                     <div class="productocart">
-//                         <img src="${json.image}" alt="">
-//                         <div class='subproductocart'>
-//                             <h1>${json.title}</h1>
-//                             <p>precio: ${json.price}</p>
-//                         </div>
-//                     </div>
-//                     `;
-    
-//                     contenedor_cart.innerHTML += product
-//                 })
-//     })
-//     finalizar.addEventListener('click', () => {
-//         localStorage.removeItem('miCarrito')
-//         contenedor_cart.innerHTML = 'Su carrito esta vacio';
-//         alert('Gracias por su compra')
-//     })
-// } else {
-//     contenedor_cart.innerHTML = 'Su carrito esta vacio'
-// }
-
-
-
 if (recuperoStorage !== null) {
     for (let i = 0; i < storageToParse.length; i++) {
         let id = storageToParse[i];
